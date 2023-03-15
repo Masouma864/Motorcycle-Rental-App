60912b65ac30bf4dbf7c6d83f99b6e81be49b2de
masouma <masouma2020@gmail.com>
2023-03-15 23:02:06 +0430

    Add browser router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+</Provider>,
+document.getElementById('root'),
 );
 
 
