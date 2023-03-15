dbc9cec8246be0faac01ec56034d34e23e8b60f2
masouma <masouma2020@gmail.com>
2023-03-15 23:16:02 +0430

    add sidebar component and import bootstrap to index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import SideBar from './components/sidebar/SideBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="d-flex flex-row">
+        <SideBar />
+        <div className="bg-primary">Hello</div>
+      </div>
     </div>
   );
 }
