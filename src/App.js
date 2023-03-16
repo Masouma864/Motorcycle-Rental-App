7cea319de94b321aa48a75d806caa32b9a2d206e
masouma <masouma2020@gmail.com>
2023-03-16 17:59:03 +0430

    render delete motorcycle in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import ReservationForm from './components/reservationForm/ReservationForm';
 import {AddMotorcycle} from './components/addMotorcycle/AddMotorcycle';
+import {AddMotorcycle} from './components/deleteMotorcycle/deletemotorcycle';
 import { Routes, Route } from 'react-router-dom';
 import Motorcycles from './pages/motorcycles/motorcycles';
 
@@ -13,6 +14,8 @@ function App() {
       </div>
       <Routes>
         <Route path="/add-motorcycle" element={<AddMotorcycle />} />
+        <Route path="/" element={<Motorcycles />} />
+        <Route path="/delete-motorcycle" element={<RemoveMotorcycle />} />
       </Routes>
     </div>
   );
