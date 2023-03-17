a3a5fa2d29219e09e3e244d6cf225f8a650d34d3
masouma <masouma2020@gmail.com>
2023-03-17 09:19:28 +0430

    Add login page

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 import createMotorcycleReducer from '../components/motorcycle/motorcycle';
 import deleteMotorcycleReducer from '../components/deleteMotorcycle/deletemotorcycle';
 import reservationReducer from './reservations/reservation';
+import { authReducer } from './auth/auth';
+
+const token = localStorage.getItem('token');
+const initialState = {
+  auth: {
+    token: token || null,
+    isAuthenticated: !!token,
+  },
+};
+
 
 export default configureStore({
     reducer: {
         newMotorcycle: createMotorcycleReducer,
         delete:deleteMotorcycleReducer,
+        preloadedState: initialState,
         reservation:reservationReducer
     },
 });
