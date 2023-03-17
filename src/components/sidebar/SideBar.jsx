90630ed11996cb4fa98735bba4a29f5c1c142654
masouma <masouma2020@gmail.com>
2023-03-17 14:26:08 +0430

    Adjust sidebar

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -3,24 +3,26 @@ import React, { useState } from 'react';
 import * as RxIcons from 'react-icons/rx';
 import Links from './Links/Links';
 
-const SideBar = () => (
+const toggleSidebar = () => {
+    setSidebar(!sidebar);
+    const toggleBtn = document.querySelector('.toggle-sidebar-btn');
+    toggleBtn.classList.toggle('move');
+  };
+  return (
     <div className="position-relative">
-    <div
-      className={`sidebar-container d-flex flex-column justify-content-between bg-white p-3 ${
-        sidebar ? 'sidebar-active' : ''
-      }`}
-    >
-      <h1 className="sidebar-title pt-3 ml-5">WheelWizard</h1>
-      <Links />
-      <SocialLinks />
-    </div>
-    <button
-        type="button"
-        className="toggle-sidebar-btn"
-        onClick={toggleSidebar}
-      >
-       <RxIcons.RxHamburgerMenu size="2.5em" />
-          </button>
+      {sidebar && (
+        <div
+          className={`position-relative sidebar-container d-flex flex-column justify-content-between bg-white p-3 ${
+            sidebar ? 'sidebar-active' : ''
+          }`}
+        >
+          <h1 className="sidebar-title pt-3 ml-5">WheelWizard</h1>
+          <Links />
+          <SocialLinks />
         </div>
+      )}
+      <button
+        type="button"
+        className="toggle-sidebar-btn"/>
 );
 export default SideBar;
