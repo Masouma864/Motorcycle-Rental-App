3bd5bfc81169582e6345ace76d1453dd667b5bfb
masouma <masouma2020@gmail.com>
2023-03-18 14:00:41 +0430

    render toast container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import './App.css';
 import { useSelector } from 'react-redux';
 import AuthenticatedRoute from './routes/AuthenticatedRoute';
 import UnAuthenticatedRoute from './routes/UnAuthenticatedRoute';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <div className="app">
-
+<ToastContainer theme="colored" position="bottom-center" />
       {!isAuthenticated ? (
         <UnAuthenticatedRoute />
       ) : (
