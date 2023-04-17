9ee8fbbe403396a96350ad04121bcf4d88ced759
masouma <masouma2020@gmail.com>
2023-04-17 08:47:48 +0430

    fix errors

--- src/pages/motorcycles/motorcycleCard/motorcycleCard.jsx
+++ src/pages/motorcycles/motorcycleCard/motorcycleCard.jsx
@@ -5,14 +5,14 @@ import PropTypes from 'prop-types';
 import * as FaIcons from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
-const CarCard = ({ car }) => (
-  <Link className="link" to={`/details/${car.id}`}>
-    <div className="car-card d-flex flex-column p-5 position-relative">
-      <img src={car.image_url} className="car-image d-flex flex-column" alt="img" />
-      <div style={{ backgroundColor: '#afacac' }} className="back-for-cars" />
-      <h2 style={{ margin: '3rem' }} className="car-name">{car.name}</h2>
-      <p style={{ color: 'rgb(182 183 184)' }}>{car.description}</p>
-      <div className="cars-icons d-flex flex-row">
+const MotorcycleCard = ({ motorcycle }) => (
+  <Link className="link" to={`/details/${motorcycle.id}`}>
+    <div className="motorcycle-card d-flex flex-column p-5 position-relative">
+      <img src={motorcycle.image_url} className="motorcycle-image d-flex flex-column" alt="img" />
+      <div style={{ backgroundColor: '#afacac' }} className="back-for-motorcycles" />
+      <h2 style={{ margin: '3rem' }} className="motorcycle-name">{motorcycle.name}</h2>
+      <p style={{ color: 'rgb(182 183 184)' }}>{motorcycle.description}</p>
+      <div className="motorcycles-icons d-flex flex-row">
         <IconContext.Provider value={{ color: '#c3c0c0' }}>
           <FaIcons.FaTwitter />
           <FaIcons.FaFacebookF />
@@ -23,8 +23,8 @@ const CarCard = ({ car }) => (
   </Link>
 );
 
-CarCard.propTypes = ({
-  car: PropTypes.object,
+MotorcycleCard.propTypes = ({
+  motorcycle: PropTypes.object,
 }).isRequired;
 
-export default CarCard;
+export default MotorcycleCard;
